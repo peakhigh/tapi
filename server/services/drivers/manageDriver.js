34b98aa2bda1e2f1593177f214a24fd7af3c544a
leelakrishna <klkrishna.cse@gmail.com>
2017-03-03 08:32:41 +0530

    manage Driver changes

--- server/services/drivers/manageDriver.js
+++ server/services/drivers/manageDriver.js
@@ -6,52 +6,16 @@ module.exports = {
    type: 'form',
    requestType: 'get',
    schemaFields: ['firstname', 'lastname', 'address', 'contact', 'photo', 'idproof'], // pick fields configuration from default schema
-   schemaOverrideFeilds: {
-
-   },
    get: {
-      preValidate: (serviceConfig, req, options, cb) => {//on init hook, will get executed on service request - init
-         console.log('get prevalidate');
-         if (!req.params.id) {
-            return cb('Invalid Request');//if error, return as first argument
-         }
-         return cb();
-      },
-      callback: (schema, serviceConfig, req, options, cb) => {//callback hook  - after serving the request - forms & grid
-         console.log('get callback', req.params.id);
+       callback: (schema, serviceConfig, req, options, cb) => {//callback hook  - after serving the request - forms & grid
+         console.log('get callback');
          const model = require('mongoose').model(collection);
-         if (req.params.id) {
-             model.getById(req.params, {
-               response: schema
-            }, cb);
-            //cb(null, schema);
-         } else {
-            cb(null, schema);
-         }
-      }
-   },
-   post: {
-      preValidate: (serviceConfig, req, options, cb) => { //on post - validate, will get executed on POST service request
-         console.log('post prevalidate');
-         if (!req.body._id) {
-            return cb('Invalid Request');//if error, return as first argument
-         }
-         return cb();
-      },
-      callback: (serviceConfig, req, options, cb) => { //callback hook  for post request
-         console.log('post callback');
-         const model = require('mongoose').model(collection);
-
-         console.log(req.body);
-        
-         if (req.body._id) {
-            let data = {
-               _id: req.body._id
-            };            
-            model.editById(data, null, cb);
-         } else {
-            cb({});
-         }
+         model.listFields(req.query, {
+            selectFields: serviceConfig.schemaFields,
+            response: {
+               schema: schema
+            }
+         }, cb);
       }
    }
-};
+  };
